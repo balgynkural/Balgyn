e66cc6ba08bd2d907fe4c85ec106799107e114ee
Balgyn <135622773+balgynkural@users.noreply.github.com>
2023-06-07 13:52:19 +0600

    Update vigenere.cpp

--- vigenere.cpp
+++ vigenere.cpp
@@ -1,44 +1,74 @@
 #include <iostream>
 #include <string>
+
 using namespace std;
 
 string encryptVigenere(string plaintext, string key) {
-    string ciphertext;
-    int keyLength = key.length();
+    string ciphertext = "";
+    int keyLen = key.length();
 
-    for (int i = 0; i < plaintext.length(); ++i) {
+    for (int i = 0; i < plaintext.length(); i++) {
         char plainChar = plaintext[i];
-        char keyChar = key[i % keyLength];
-        char encryptedChar = 'A' + (plainChar + keyChar) % 26;
-        ciphertext += encryptedChar;
+        char keyChar = key[i % keyLen];
+
+        if (isalpha(plainChar)) {
+            int plainIndex = tolower(plainChar) - 'a';
+            int keyIndex = tolower(keyChar) - 'a';
+
+            char encryptedChar = 'a' + (plainIndex + keyIndex) % 26;
+
+            if (isupper(plainChar))
+                encryptedChar = toupper(encryptedChar);
+
+            ciphertext += encryptedChar;
+        } else {
+            ciphertext += plainChar;
+        }
     }
 
     return ciphertext;
 }
 
 string decryptVigenere(string ciphertext, string key) {
-    string plaintext;
-    int keyLength = key.length();
+    string plaintext = "";
+    int keyLen = key.length();
 
-    for (int i = 0; i < ciphertext.length(); ++i) {
-        char encryptedChar = ciphertext[i];
-        char keyChar = key[i % keyLength];
-        char decryptedChar = 'A' + (encryptedChar - keyChar + 26) % 26;
-        plaintext += decryptedChar;
+    for (int i = 0; i < ciphertext.length(); i++) {
+        char cipherChar = ciphertext[i];
+        char keyChar = key[i % keyLen];
+
+        if (isalpha(cipherChar)) {
+            int cipherIndex = tolower(cipherChar) - 'a';
+            int keyIndex = tolower(keyChar) - 'a';
+
+            char decryptedChar = 'a' + (cipherIndex - keyIndex + 26) % 26;
+
+            if (isupper(cipherChar))
+                decryptedChar = toupper(decryptedChar);
+
+            plaintext += decryptedChar;
+        } else {
+            plaintext += cipherChar;
+        }
     }
 
     return plaintext;
 }
 
 int main() {
-    string plaintext = "HELLO";
-    string key = "KEY";
+    string plaintext, key;
 
-    string encryptedText = encryptVigenere(plaintext, key);
-    cout << "Encrypted Text: " << encryptedText << endl;
+    cout << "Enter the plaintext: ";
+    getline(cin, plaintext);
 
-    string decryptedText = decryptVigenere(encryptedText, key);
-    cout << "Decrypted Text: " << decryptedText << endl;
+    cout << "Enter the key: ";
+    getline(cin, key);
+
+    string ciphertext = encryptVigenere(plaintext, key);
+    cout << "Encrypted ciphertext: " << ciphertext << endl;
+
+    string decryptedText = decryptVigenere(ciphertext, key);
+    cout << "Decrypted plaintext: " << decryptedText << endl;
 
     return 0;
 }
